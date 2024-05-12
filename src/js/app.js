d15b07b4bcf1b7044199ace5e81c41d05edf8264
4rchi-nv <arslan0agajanov@gmail.com>
2024-05-12 10:37:01 +0500

    12.05

--- src/js/app.js
+++ src/js/app.js
@@ -1,2 +1,17 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import '../css/style.css';
+import UI from './views/ui';
+import { validate } from './helpers/validate';
+
+const { form, email, password } = UI;
+
+//events
+form.addEventListener('submit', e => {
+    e.preventDefault();
+    onSubmit();
+});
+
+//handlers
+function onSubmit() {
+    
+}
---
+++ src/js/views/ui.js
@@ -0,0 +1,7 @@
+const UI = {
+    form: document.forms['loginForm'],
+    email: document.getElementById('email'),
+    password: document.getElementById('password'),
+};
+
+export default UI;
